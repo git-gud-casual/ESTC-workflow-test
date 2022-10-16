05b1deffc5c65792a8432bc494b7c9c4192befcc
ppsn-code <popasena2@ya.ru>
2022-10-16 01:32:06 +0000

    Check allocated vector is not NULL
    Closes #3

--- src/vector3/vector3.c
+++ src/vector3/vector3.c
@@ -5,21 +5,31 @@
 vector3* sum(const vector3* vector1, const vector3* vector2) {
     vector3* sum_vector = malloc(sizeof(vector3));
 
-    sum_vector->x = vector1->x + vector2->x;
-    sum_vector->y = vector1->y + vector2->y;
-    sum_vector->z = vector1->z + vector2->z;
+    if (sum_vector != NULL) {
+        sum_vector->x = vector1->x + vector2->x;
+        sum_vector->y = vector1->y + vector2->y;
+        sum_vector->z = vector1->z + vector2->z;
 
-    return sum_vector;
+        return sum_vector;
+    }
+    else {
+        return NULL;
+    }
 }
 
 vector3* sub(const vector3* vector1, const vector3* vector2) {
     vector3* sub_vector = malloc(sizeof(vector3));
 
-    sub_vector->x = vector1->x - vector2->x;
-    sub_vector->y = vector1->y - vector2->y;
-    sub_vector->z = vector1->z - vector2->z;
+    if (sub_vector != NULL) {
+        sub_vector->x = vector1->x - vector2->x;
+        sub_vector->y = vector1->y - vector2->y;
+        sub_vector->z = vector1->z - vector2->z;
 
-    return sub_vector;
+        return sub_vector;
+    }
+    else {
+        return NULL;
+    }
 }
 
 int dot(const vector3* vector1, const vector3* vector2) {
@@ -35,11 +45,16 @@ int dot(const vector3* vector1, const vector3* vector2) {
 vector3* cross(const vector3* vector1, const vector3* vector2) {
     vector3* cross_vector = malloc(sizeof(vector3));
 
-    cross_vector->x = vector1->y * vector2->z - vector1->z * vector2->y;
-    cross_vector->y = vector1->z * vector2->x - vector1->x * vector2->z;
-    cross_vector->z = vector1->x * vector2->y - vector1->y * vector2->x;
+    if (cross_vector != NULL) {
+        cross_vector->x = vector1->y * vector2->z - vector1->z * vector2->y;
+        cross_vector->y = vector1->z * vector2->x - vector1->x * vector2->z;
+        cross_vector->z = vector1->x * vector2->y - vector1->y * vector2->x;
 
-    return cross_vector;
+        return cross_vector;
+    }
+    else {
+        return NULL;
+    }
 }
 
 void print_vector3(const vector3* vector) {
